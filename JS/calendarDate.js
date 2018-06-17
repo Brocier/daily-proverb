c563a68899774a209ac7571fdbd07a3321d52d0d
Brocier <jschoe04@gmail.com>
2018-06-17 01:05:49 -0400

    Use switch statements to get an accurate date on every page load

--- JS/calendarDate.js
+++ JS/calendarDate.js
@@ -1,6 +1,84 @@
 $(() => {
-  console.log(`It's working!`)
+  console.log(`https://media.giphy.com/media/GIEXgLDfghUSQ/giphy.gif`)
 
-  const today = Date.now()
+  let today = new Date()
+  let dd = today.getDate()
+  let mm = today.getMonth() + 1
+  const yyyy = today.getFullYear()
+  if (dd < 10) {
+    dd = "0" + dd
+  }
+  if (mm < 10) {
+    mm = "0" + mm
+  }
+
+  switch (today.getMonth()) {
+    case 0:
+      month = "January"
+      break;
+    case 1:
+      month = "February"
+      break;
+    case 2:
+      month = "March"
+      break;
+    case 3:
+      month = "April"
+      break;
+    case 4:
+      month = "May"
+      break;
+    case 5:
+      month = "June"
+      break;
+    case 6:
+      month = "July"
+      break;
+    case 7:
+      month = "August"
+      break;
+    case 8:
+      month = "September"
+      break;
+    case 9:
+      month = "October"
+      break;
+    case 10:
+      month = "November"
+      break;
+    case 11:
+      month = "December"
+      break;
+    default:
+      text = "No value found";
+  }
+
+  today = `${month} ${dd}, ${yyyy}`
   console.log(today)
+
+  switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+  $(".dateOfToday").append(`${day}, ${today}`);
+
 })
